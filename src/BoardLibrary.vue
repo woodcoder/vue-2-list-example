cdfea75ceef76fd4a54d341fe62c155ba34bd6f1
Andy Wood <andy@ideaflip.com>
2016-10-04 13:19:27 +0100

    Use a computed version of the list to implement sortable columns

--- src/BoardLibrary.vue
+++ src/BoardLibrary.vue
@@ -6,14 +6,14 @@
     <table v-else>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Name</th>
+          <board-sort v-model="sort" prop="type">Type</board-sort>
+          <board-sort v-model="sort" prop="name">Name</board-sort>
           <th></th>
         </tr>
       </thead>
       <tbody>
-        <board-game v-for="(game, index) in games" :type="game.type" :name="game.name">
-          <board-delete :index="index" @delete="handleDelete"></board-delete>
+        <board-game v-for="game in sorted" :type="game.type" :name="game.name">
+          <board-delete :index="game.index" @delete="handleDelete"></board-delete>
         </board-game>
       </tbody>
     </ul>
@@ -21,19 +21,29 @@
 </template>
 
 <script>
+var _ = require('lodash')
 var BoardAdd = require('./BoardAdd.vue')
 var BoardGame = require('./BoardGame.vue')
 var BoardDelete = require('./BoardDelete.vue')
+var BoardSort = require('./BoardSort.vue')
 
 module.exports = {
   data: function() {
     return {
+      sort: 'type',
       games: []
     }
   },
   computed: {
     empty: function() {
       return this.games.length == 0
+    },
+    sorted: function() {
+      // update each game with its index in the array
+      this.games.forEach(function (value, index) {
+        value.index = index
+      })
+      return _.sortBy(this.games, [this.sort])
     }
   },
   methods: {
@@ -42,12 +52,13 @@ module.exports = {
     },
     handleDelete: function (index) {
       this.games.splice(index, 1)
-    }
+    },
   },
   components: {
     BoardAdd: BoardAdd,
     BoardGame: BoardGame,
-    BoardDelete: BoardDelete
+    BoardDelete: BoardDelete,
+    BoardSort: BoardSort
   }
 }
 </script>
@@ -55,5 +66,13 @@ module.exports = {
 <style>
   table {
     width: 100%;
+    table-layout: fixed;
+  }
+  table th:nth-of-type(1),
+  table th:nth-of-type(3) {
+    width: 10%;
+  }
+  table th:nth-of-type(2) {
+    width: 40%;
   }
 </style>
---
+++ src/BoardSort.vue
@@ -0,0 +1,20 @@
+<template>
+  <th @click="sort">
+    <slot></slot>
+    <span v-if="value == prop" class="icon-arrow-down2"></span>
+  </th>
+</template>
+
+<script>
+module.exports = {
+  props: [
+    'prop',
+    'value'
+  ],
+  methods: {
+    sort: function () {
+      this.$emit('input', this.prop)
+    }
+  }
+}
+</script>
