a72c50996eb969d55283a6a13540dc93ea437d97
Andy Wood <andy@ideaflip.com>
2016-10-10 08:48:06 +0100

    Switch back to the Vue.js runtime-only build, as per the normal Vue 2.0 scaffold.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var Vue = require('vue')
 var BoardLibrary = require('./BoardLibrary.vue')
 
-Vue.component('board-library', BoardLibrary)
-var app = new Vue({
-  el: '#app'
+new Vue({
+  el: '#app',
+  render: h => h(BoardLibrary)
 })
